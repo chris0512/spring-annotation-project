1859f73746b47d83d07dc9300d48aa54d1636270
chris0512 <kyang3200@gmail.com>
2022-05-26 19:09:48 +0800

    injecting value from properties

---
+++ src/com/chris/spring_annotation_project/SadFortuneService.java
@@ -0,0 +1,11 @@
+package com.chris.spring_annotation_project;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		
+		return "Today is a sad day";
+	}
+
+}
--- src/com/chris/spring_annotation_project/SportConfig.java
+++ src/com/chris/spring_annotation_project/SportConfig.java
@@ -1,10 +1,24 @@
 package com.chris.spring_annotation_project;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("com.chris.spring_annotation_project")
+//@ComponentScan("com.chris.spring_annotation_project")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
+	
+	// define bean for our sad fortune service
+	@Bean 
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach and inject dependency
+	@Bean 
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 
 }
